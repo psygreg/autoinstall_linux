b12e4ee6cb1f052ed02068b2ebd7a01faddb748b
psygreg <48108858+psygreg@users.noreply.github.com>
2024-07-22 18:27:57 -0300

    autoinstall v3
    release 3

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 ##VARS
 #vars_bundles
-BASE="sudo pacman -S --needed --noconfirm chromium timeshift libreoffice-fresh pinta wine wine-gecko wine-mono freetype2 lib32-alsa-lib lib32-alsa-plugins lib32-libpulse pipewire-pulse lib32-pipewire lib32-openal flatpak v4l2loopback-dkms v4l2loopback-utils v4l-utils pipewire-v4l2 lib32-pipewire-v4l2 && flatpak install -y --noninteractive --or-update com.github.IsmaelMartinez.teams_for_linux"
-BASEYAY="yay --noconfirm && yay -S --needed --noconfirm pamac-all appimagelauncher debtap"
-GAME="sudo pacman -S --needed --noconfirm timeshift lib32-vkd3d vkd3d gamemode lib32-gamemode wine wine-gecko wine-mono pipewire-pulse pulseaudio-alsa freetype2 lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-pipewire lib32-openal flatpak v4l2loopback-dkms v4l2loopback-utils v4l-utils pipewire-v4l2 lib32-pipewire-v4l2 mangohud gamescope goverlay && flatpak install -y --noninteractive --or-update com.obsproject.Studio net.lutris.Lutris org.prismlauncher.PrismLauncher com.valvesoftware.Steam com.heroicgameslauncher.hgl io.github.unknownskl.greenlight com.discordapp.Discord com.valvesoftware.Steam.VulkanLayer.MangoHud org.freedesktop.Platform.VulkanLayer.MangoHud com.valvesoftware.Steam.Utility.MangoHud"
-GAMEYAY="yay --noconfirm && yay -S --needed --noconfirm pamac-all xone-dkms hydra-launcher-bin appimagelauncher debtap"
+BASEPACMAN="sudo pacman -S --needed --noconfirm chromium timeshift libreoffice-fresh pinta wine wine-gecko wine-mono freetype2 flatpak v4l2loopback-dkms v4l2loopback-utils v4l-utils pipewire-v4l2 lib32-pipewire-v4l2"
+BASEFLAT="flatpak install -y --noninteractive --or-update com.github.IsmaelMartinez.teams_for_linux"
+BASEAURA="aura --noconfirm && aura -A --needed --noconfirm appimagelauncher debtap"
+GAMEPACMAN="sudo pacman -S --needed --noconfirm timeshift lib32-vkd3d vkd3d gamemode lib32-gamemode wine wine-gecko wine-mono freetype2 flatpak v4l2loopback-dkms v4l2loopback-utils v4l-utils pipewire-v4l2 lib32-pipewire-v4l2 mangohud gamescope goverlay"
+GAMEFLAT="flatpak install -y --noninteractive --or-update net.lutris.Lutris org.prismlauncher.PrismLauncher com.valvesoftware.Steam com.heroicgameslauncher.hgl io.github.unknownskl.greenlight com.discordapp.Discord com.valvesoftware.Steam.VulkanLayer.MangoHud org.freedesktop.Platform.VulkanLayer.MangoHud com.valvesoftware.Steam.Utility.MangoHud"
+GAMEAURA="aura --noconfirm && aura -A --needed --noconfirm xpad-neo-dkms appimagelauncher debtap obs-studio-tytan652"
 #vars_gpu
-NVIDIA="cd && git clone https://github.com/Frogging-Family/nvidia-all.git && cd nvidia-all && makepkg -si && cd && rm -r ~/nvidia-all"
-PRIME="sudo pacman -S --needed --noconfirm nvidia-prime"
-GPUS=$(lspci | grep VGA | wc -l)
+#NVIDIA="cd && git clone https://github.com/Frogging-Family/nvidia-all.git && cd nvidia-all && makepkg -si && cd && rm -r ~/nvidia-all"
+#PRIME="sudo pacman -S --needed --noconfirm nvidia-prime"
+#GPUS=$(lspci | grep VGA | wc -l)
 ##FUNCTIONS
 #get language from OS
 get_lang() {
@@ -30,22 +32,26 @@ choose_bundle() {
 }
 #basic bundle
 basic_bundle() {            
-    eval "$BASE"
-    yay_func
-    eval "$BASEYAY"
+    eval "$BASEPACMAN"
+    eval "$BASEFLAT"
+    aura_func
+    eval "$BASEAURA"
 }
 #gamer bundle
 gamer_bundle() {
-	eval "$GAME"
-    yay_func
-    eval "$GAMEYAY"
+	eval "$GAMEPACMAN"
+    eval "$GAMEFLAT"
+    aura_func
+    eval "$GAMEAURA"
 }
 #yay function
-yay_func() {
-	if pacman -Qs yay > /dev/null; then
-        echo "YAY already installed, proceeding..."
+aura_func () {
+    if pacman -Qs aura > /dev/null; then
+        echo "Aura already installed, proceeding..."
     else
-        cd && pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd && rm -r ~/yay
+        git clone https://aur.archlinux.org/aura-bin.git
+        cd aura-bin || return
+        makepkg -Si
     fi
 }
 #gpu detect function
@@ -74,82 +80,90 @@ radeon_vlk() {
     fi
 }
 ##SCRIPT RUN START
-#check if OS is arch-based by looking for pacman
-if command -v pacman &> /dev/null; then
-    echo "Pacman detected. Starting..."
+#root checker
+if (( ! UID )); then
+	echo "Do not run AutoInstall as root."
+	exit 1
 else
-    echo "Pacman not detected."
-    exit 1
-fi
-#get language
-get_lang
-#en-US
-if [ "$ulang" == "en" ]; then
-    echo "This is the *Psygreg AutoInstall Script*."
-    echo "It will perform a complete system update, and install required dependencies, drivers and applications to your Arch-based Linux system."
-    echo "If all programs are already installed, it will just perform the system update and create a system restore point through Timeshift." 
-    #detect nvidia GPU
-    gpu_detect
-    if [[ $GPU == *' nvidia '* ]]; then
-        printf 'Nvidia GPU is present:  %s\n' "$GPU"
-        echo "Would you like to install or update to the latest Nvidia proprietary drivers?"
-        choose_nvidia
-        if [ "$drv" == "1" ]; then
-            eval "$NVIDIA"
-            if [ "$GPUS" -gt 1 ]; then
-                eval "$PRIME"
-            else
-                echo "Single GPU system, PRIME not required. Skipping..."
-            fi
-        else
-            echo "Skipping Nvidia drivers..."
-        fi
+#check if OS is arch-based by looking for pacman
+    if command -v pacman &> /dev/null; then
+        echo "Pacman found. Starting..."
     else
-        echo "Nvidia GPU not detected."
+        echo "Pacman not found."
+        exit 2
     fi
-    #radeon fix
-    radeon_vlk
-    #bundle install
-    echo "Which bundle do you wish to install?"
-    choose_bundle
+#get language
+    get_lang
+#en-US
+    if [ "$ulang" == "en" ]; then
+        echo "This is the *Psygreg AutoInstall Script*."
+        echo "It will perform a complete system update, and install required dependencies, drivers and applications to your Arch-based Linux system."
+        echo "If all programs are already installed, it will just perform the system update and create a system restore point through Timeshift." 
+        #DISABLED NVIDIA GPU BC BUGS DONT MESS AROUND WITH THIS FOR NOW
+        #detect nvidia GPU
+        #gpu_detect
+        #if [[ $GPU == *' nvidia '* ]]; then
+            #printf 'Nvidia GPU is present:  %s\n' "$GPU"
+            #echo "Would you like to install or update to the latest Nvidia proprietary drivers?"
+            #choose_nvidia
+            #if [ "$drv" == "1" ]; then
+                #eval "$NVIDIA"
+                #if [ "$GPUS" -gt 1 ]; then
+                    #eval "$PRIME"
+                #else
+                    #echo "Single GPU system, PRIME not required. Skipping..."
+                #fi
+            #else
+                #echo "Skipping Nvidia drivers..."
+            #fi
+        #else
+            #echo "Nvidia GPU not detected."
+        #fi
+        ##RADEON FIX
+        radeon_vlk
+        ##BUNDLE INSTALL
+        echo "Which bundle do you wish to install?"
+        choose_bundle
         if [ "$bundle" == "1" ]; then
             basic_bundle
         elif [ "$bundle" == "2" ]; then
             gamer_bundle
         else
-            echo "Operation cancelled."
+            "Operation cancelled."
             exit 0
         fi
-    echo "Script Psygreg AutoInstall has finished successfully. Reboot to apply all changes."
+        echo "Script Psygreg AutoInstall has finished successfully. Reboot to apply all changes."
+        exit 0
 #pt-BR
-elif [ "$ulang" == "pt" ]; then
-    echo "Este é o script *Psygreg AutoInstall*."
-    echo "Ele atualiza completamente o sistema, instala todos os aplicativos, drivers e dependências necessárias para seu sistema Linux baseado em Arch."
-    echo "Se todos os programas já tiverem sido instalados, ele só irá fazer uma atualização completa do sistema e criará um ponto de restauração quando finalizar." 
-    #detect nvidia GPU
-    gpu_detect
-    if [[ $GPU == *' nvidia '* ]]; then
-        printf 'GPU Nvidia detectada:  %s\n' "$GPU"
-        echo "Você gostaria de instalar ou atualizar os drivers proprietários Nvidia?"
-        choose_nvidiabr
-        if [ "$drv" == "1" ]; then
-            eval "$NVIDIA"
-            if [ "$GPUS" -gt 1 ]; then
-                eval "$PRIME"
-            else
-                echo "Sistema sem modo GPU híbrida, pulando..."
-            fi
-        else
-            echo "Pulando drivers Nvidia..."
-        fi
-    else
-        echo "GPU Nvidia não detectada."
-    fi
-    #radeon fix
-    radeon_vlk
-    #bundle install
-    echo "Qual pacote deseja instalar?"
-    choose_bundle
+    elif [ "$ulang" == "pt" ]; then
+        echo "Este é o script *Psygreg AutoInstall*."
+        echo "Ele atualiza completamente o sistema, instala todos os aplicativos, drivers e dependências necessárias para seu sistema Linux baseado em Arch."
+        echo "Se todos os programas já tiverem sido instalados, ele só irá fazer uma atualização completa do sistema e criará um ponto de restauração quando finalizar." 
+        #DISABLED NVIDIA GPU BC BUGS DONT MESS AROUND WITH THIS FOR NOW
+        #detect nvidia GPU
+        #gpu_detect
+        #if [[ $GPU == *' nvidia '* ]]; then
+            #printf 'GPU Nvidia detectada:  %s\n' "$GPU"
+            #echo "Você gostaria de instalar ou atualizar os drivers proprietários Nvidia?"
+            #choose_nvidiabr
+            #if [ "$drv" == "1" ]; then
+                #eval "$NVIDIA"
+                #if [ "$GPUS" -gt 1 ]; then
+                    #eval "$PRIME"
+                #else
+                    #echo "Sistema sem modo GPU híbrida, pulando..."
+                #fi
+            #else
+                #echo "Pulando drivers Nvidia..."
+            #fi
+        #else
+            #echo "GPU Nvidia não detectada."
+        #fi
+        ##RADEON FIX
+        radeon_vlk
+        ##BUNDLE INSTALL
+        echo "Qual pacote deseja instalar?"
+        choose_bundle
         if [ "$bundle" == "1" ]; then
             basic_bundle
         elif [ "$bundle" == "2" ]; then
@@ -158,5 +172,7 @@ elif [ "$ulang" == "pt" ]; then
             echo "Operação cancelada."
             exit 0
         fi
-    echo "Script Psygreg AutoInstall concluiu com sucesso. Reinicie para aplicar as alterações."
+        echo "Script Psygreg AutoInstall concluiu com sucesso. Reinicie para aplicar as alterações."
+        exit 0
+    fi
 fi
